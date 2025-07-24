fd2a3d109fe2d8894923d151b3d1cce5f35b48d6
keiseoi <79731911+keiseoi@users.noreply.github.com>
2025-07-24 23:18:04 +0800

    更新 wxUpdateScript.js

--- wxUpdateScript.js
+++ wxUpdateScript.js
@@ -1,18 +1,17 @@
-// weread_patch.js
-// https://raw.githubusercontent.com/keiseoi/weread_UpdateBlock/refs/heads/main/wxUpdateScript.js
+// weread_patch_safe.js
 if ($response.body) {
   let body = JSON.parse($response.body);
 
-  // 关闭强制更新弹窗
+  // 只改动已知与强制更新相关的字段
   body.upgrade = 0;
   body.notice_title = "";
   body.notice_msg = "";
 
   if (body.configsets) {
-    body.configsets.upgrade = 0;
-    body.configsets.notice_type = 0;
-    body.configsets.notice_msg = "";
-    body.configsets.notice_title = "";
+    if (typeof body.configsets.upgrade === "number") body.configsets.upgrade = 0;
+    if (typeof body.configsets.notice_type === "number") body.configsets.notice_type = 0;
+    if (typeof body.configsets.notice_msg === "string") body.configsets.notice_msg = "";
+    if (typeof body.configsets.notice_title === "string") body.configsets.notice_title = "";
   }
 
   $done({ body: JSON.stringify(body) });
